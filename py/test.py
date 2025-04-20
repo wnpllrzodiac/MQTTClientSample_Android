3feb6455725395554a33b3098703099676f4a700
Michael Ma <wnpllr@gmail.com>
2025-04-20 23:28:18 +0800

    py

--- py/test.py
+++ py/test.py
@@ -28,8 +28,9 @@ message_payload = {
     ]
 }
 
-def on_connect(client, userdata, flags, reason_code, properties):
+def on_connect(client, userdata, flags, reason_code): #, properties):
     global pub_mode
+    global SUBSCRIBE_TOPIC2
     print(f"Connected with result code {reason_code}")
 
     # 连接结果代码（result code）
@@ -45,8 +46,8 @@ def on_connect(client, userdata, flags, reason_code, properties):
         print("Connection successful")
         # 在这里可以进行其他初始化操作，比如订阅主题等
         if not pub_mode:
-            client.subscribe(SUBSCRIBE_TOPIC2, qos=2)
-            print('subscribe: ' + SUBSCRIBE_TOPIC2)
+            client.subscribe(PUBLISH_TOPIC)
+            print('subscribe: ' + PUBLISH_TOPIC)
     else:
         print("Connection failed")
 
@@ -54,7 +55,8 @@ def on_message(client, userdata, message):
     print(f"Received message from topic '{message.topic}': {message.payload.decode()}")
 
 def publish_message():
-    client.publish(PUBLISH_TOPIC, json.dumps(message_payload))
+    res = client.publish(topic=PUBLISH_TOPIC, payload=json.dumps(message_payload))
+    print(res)
     print(f"Published message: {json.dumps(message_payload)}")
     # 设置定时器，以继续定期发布消息
     Timer(PUBLISH_INTERVAL, publish_message).start()
@@ -66,22 +68,24 @@ if len(sys.argv) > 1 and sys.argv[1] == 'pub':
     
 MQTT_SERVER = '117.78.5.125'
 MQTT_PORT = 1883
-#CLIENT_ID = '67fcbde85367f573f782aca1_dev1_0_0_2025041506'
-CLIENT_ID = '67fcbde85367f573f782aca1_dev1_0_0' + str(time.time())
+CLIENT_ID = '67fcbde85367f573f782aca1_dev1_0_0_2025041506'
 USERNAME = '67fcbde85367f573f782aca1_dev1'
 PASSWORD = 'aa69017f3c786a9a247dad3befc896b0b56478cc46758b5d4573a4710fadd212'
 SUBSCRIBE_TOPIC = "$oc/devices/67fcbde85367f573f782aca1_dev1/sys/messages/down"
-SUBSCRIBE_TOPIC2 = "$oc/devices/67fcbde85367f573f782aca1_dev1/sys/properties/report"
-PUBLISH_TOPIC = "$oc/devices/67fcbde85367f573f782aca1_dev1/sys/properties/report"
+SUBSCRIBE_TOPIC2 = "oc/devices/67fcbde85367f573f782aca1_dev1/sys/properties/report"
+PUBLISH_TOPIC = "oc/devices/67fcbde85367f573f782aca1_dev1/sys/properties/report"
 PUBLISH_INTERVAL = 10
 
-print(f'CLIENT_ID: {CLIENT_ID}')
+CLIENT_ID_2 = '67fcbde85367f573f782aca1_dev1_0_0_2025042008'
+USERNAME_2 = '67fcbde85367f573f782aca1_dev1'
+PASSWORD_2 = '17edb4a165a72059e8e205a9ec94c4f26ac2abdeaa75f5c80f32cd77d66a3c6f'
 
-client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id=CLIENT_ID)
+print(f'CLIENT_ID: {CLIENT_ID}')
+client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION1, client_id=CLIENT_ID)
 client.on_connect = on_connect
 client.on_message = on_message
 client.username_pw_set(USERNAME, PASSWORD)
-client.connect(MQTT_SERVER, MQTT_PORT, 60)
+client.connect(MQTT_SERVER, MQTT_PORT)
 if not pub_mode:
     client.loop_forever()
 
